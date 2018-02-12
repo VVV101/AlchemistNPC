0cd1dfed64a0bdb17a0a82c639a33b4333c222cf
somebody0214 <eresko02@gmail.com>
2018-02-12 17:50:08 +0500

    honey...

--- Tiles/GlobalTile.cs
+++ Tiles/GlobalTile.cs
@@ -1,1 +1,14 @@
+using Terraria;
+using Terraria.ModLoader;
 
+namespace AlchemistNPC.Tiles
+{
+    public class MateriaTransmutator : GlobalTile
+    {
+        public override bool adjTiles
+        {
+        adjTile.SetDefault(Main.LocalPlayer.adjHoney == true)
+        
+        }
+    }
+}
