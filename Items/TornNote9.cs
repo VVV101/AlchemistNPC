d6b87424579621130df2981b8256666b85a0db67
SlapitNow <38248650+SlapitNow@users.noreply.github.com>
2018-04-10 14:19:08 +0200

    Update TornNote9.cs
    Last note

--- Items/TornNote9.cs
+++ Items/TornNote9.cs
@@ -10,11 +10,11 @@ namespace AlchemistNPC.Items
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Torn Note #9");
-			Tooltip.SetDefault("'If anyone will find this, then know that will be the last note"
-			+"\nI found out that Alchemist could make Life Elixir..."
-			+"\nThe legendary brew which could increase max life of user..."
-			+"\nBut for making it, you will require to get Alchemical Bundle and find Philosopher's stone."
-			+"\nWith all that, he will brew you a portion of Life Elixir.'");
+			Tooltip.SetDefault("'If anyone finds this, then know that it's the last note"
+			+"\nI found out that the Alchemist could make a Life Elixir..."
+			+"\nA legendary brew which increases the max life of the user..."
+			+"\nBut to make it, you need an Alchemical Bundle and a Philosopher's stone."
+			+"\nWith all of that, he will brew you a portion of Life Elixir.'");
 			DisplayName.AddTranslation(GameCulture.Russian, "Изорванная записка #9");
 			Tooltip.AddTranslation(GameCulture.Russian, "'Если кто-нибудь найдёт это, то пусть знает, что это - последняя записка.\nЯ выяснил, что Алхимик способен создать Эликсир Жизни\nЭто легендарный напиток, который способен увеличить максимальный запас жизней того, кто его использует...\nНо для его создания тебе потребуется собрать Алхимический набор и найти Философкий Камень.\nПри наличии всего этого, он сварит вам порцию Эликсира Жизни.'"); 
 		}
