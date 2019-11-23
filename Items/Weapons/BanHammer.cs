19dfe7208201aa90d55c3076c7a3b3033952f7e5
VVV101 <VVVHUNTER@yandex.ru>
2019-11-23 12:46:33 +0600

    ban hammer fix

--- Items/Weapons/BanHammer.cs
+++ Items/Weapons/BanHammer.cs
@@ -52,12 +52,11 @@ namespace AlchemistNPC.Items.Weapons
 		{
 			if (target.boss == false)
 			{
-				target.buffImmune[mod.BuffType("Banned")] = false;
-				target.AddBuff(mod.BuffType("Banned"), 60);
-			}
-			if (target.type == 134 || target.type == 135 || target.type == 136 || target.type == 325 || target.type == 327 || target.type == 325 || target.type == 344 || target.type == 345 || target.type == 346)
-			{
-				damage = 88;
+				if (target.type != 134 || target.type != 135 || target.type != 136 || target.type != 325 || target.type != 327 || target.type != 325 || target.type != 344 || target.type != 345 || target.type != 346 ||  target.type != 477)
+				{
+					target.buffImmune[mod.BuffType("Banned")] = false;
+					target.AddBuff(mod.BuffType("Banned"), 60);
+				}
 			}
 			if (ModLoader.GetMod("CalamityMod") != null)
 			{
